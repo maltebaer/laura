4cd20e332f44a4048fff5fc5c6c99c287a801d09
malte.baer <malte.baer@biomes.world>
2021-12-14 22:02:29 +0100

    added alpinejs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: ["./index.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./index.html"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["'Poppins'", ...defaultTheme.fontFamily.sans],
+                kicker: ["Fredericka the Great"],
+            },
+        },
+    },
+    plugins: [],
+};
